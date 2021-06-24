696302cb2fb4011e42e899ee87742e1788872696
Vadim Belinovich <vadimbelinovich@gmail.com>
2021-06-24 10:42:25 +0300

    fixed bug with event listener

--- source/js/modules/inputFile.js
+++ source/js/modules/inputFile.js
@@ -1,28 +1,32 @@
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 const fileChooser = document.querySelector('input[type="file"]')
 const preview = document.querySelector('.file-preview');
-const removeIcon = document.querySelector('.remove-button')
+const removeIcon = document.querySelector('.remove-button');
 
-fileChooser.addEventListener('change', function() {
-  const file = fileChooser.files[0];
-  const fileName = file.name.toLowerCase();
-  
-  let matches = FILE_TYPES.some(function(it) {
-    return fileName.endsWith(it);
-  });
-  
-  if (matches) {
-    const reader = new FileReader();
-    reader.addEventListener('load', function() {
-      preview.src = reader.result;
-      removeIcon.style.display = 'flex';
+if(fileChooser) {
+
+    fileChooser.addEventListener('change', function() {
+      const file = fileChooser.files[0];
+      const fileName = file.name.toLowerCase();
+      
+      const matches = FILE_TYPES.some(function(it) {
+        return fileName.endsWith(it);
+      });
+      
+      if (matches) {
+        const reader = new FileReader();
+        reader.addEventListener('load', function() {
+          preview.src = reader.result;
+          removeIcon.style.display = 'flex';
+        })
+        reader.readAsDataURL(file);
+      }
     })
-    reader.readAsDataURL(file);
-  }
-})
+    
+    removeIcon.addEventListener('click', function() {
+        removeIcon.style.display = 'none';
+        preview.src = '';
+        fileChooser.reset();
+    })
 
-removeIcon.addEventListener('click', function() {
-    removeIcon.style.display = 'none';
-    preview.src = '';
-    fileChooser.reset();
-})
+}
